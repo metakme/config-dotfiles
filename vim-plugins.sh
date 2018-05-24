151d6faf1b4118132a8fecceb98b99d99dfbc745
metakme <the_kme@hotmail.com>
2018-05-24 13:50:25 +0200

    added vim plugins script

---
+++ vim-plugins.sh
@@ -0,0 +1,15 @@
+#install pathogen
+mkdir -p ~/vim/autoload ~/vim/bundle && \
+curl -LSso ~/vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p  ~/vim/bundle
+
+git clone https://github.com/altercation/vim-colors-solarized.git ~/vim/bundle/vim-colors-solarized
+git clone https://github.com/scrooloose/nerdtree.git ~/vim/bundle/nerdtree
+git clone https://github.com/vim-airline/vim-airline.git ~/vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/vim/bundle/vim-airline-themes
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/vim/bundle/ctrlp.vim
+git clone https://github.com/pangloss/vim-javascript.git ~/vim/bundle/vim-javascript
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/vim/bundle/syntastic
+git clone https://github.com/leafgarland/typescript-vim.git ~/vim/bundle/typescriptvim
+git clone git://github.com/digitaltoad/vim-pug.git ~/vim/bundle/vim-pug
+git clone git://github.com/OrangeT/vim-csharp.git ~/vim/bundle/vim-csharp
